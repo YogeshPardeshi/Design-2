47a664d1e21d3cdb1fa73b7ad428ac32b54e9d04
YogeshPardeshi <31638743+YogeshPardeshi@users.noreply.github.com>
2026-05-22 07:14:03 -0400

    complete design2

---
+++ MyQueue.java
@@ -0,0 +1,32 @@
+class MyQueue {
+    Stack<Integer> inSt;
+    Stack<Integer> outSt;
+
+    public MyQueue() {
+        this.inSt = new Stack<>();
+        this.outSt = new Stack<>();
+    }
+    
+    public void push(int x) {
+        inSt.push(x);
+    }
+    
+    public int pop() {
+        if(empty()) return -1;
+        peek();
+        return outSt.pop();
+    }
+    
+    public int peek() {
+        if(outSt.isEmpty()){
+            while(!inSt.isEmpty()){
+                outSt.push(inSt.pop());
+            }
+        }
+        return outSt.peek();
+    }
+    
+    public boolean empty() {
+        return inSt.isEmpty() && outSt.isEmpty();
+    }
+}
